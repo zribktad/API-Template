81a55c998a81575ec683773e1dd5411ae08c8b86
Tadeas Zribko <tadeas.zribko@quantasoft.com>
2026-03-25 15:28:28 +0100

    feat: add WolverineTypeExtensions for ErrorOr validation and implement corresponding unit tests

--- src/APITemplate.Api/Extensions/WolverineHandlerChainExtensions.cs
+++ src/APITemplate.Api/Extensions/WolverineHandlerChainExtensions.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using ErrorOr;
-using FluentValidation;
 using Wolverine.Runtime.Handlers;
 
 namespace APITemplate.Api.Extensions;
@@ -21,30 +19,4 @@ public static class WolverineHandlerChainExtensions
     ) =>
         chain.MessageType.HasValidatorIn(validatorAssembly)
         && chain.Handlers.Any(h => h.Method.ReturnType.IsErrorOrReturnType());
-
-    private static bool IsErrorOrReturnType(this Type returnType)
-    {
-        if (!returnType.IsGenericType)
-            return false;
-
-        var genericTypeDefinition = returnType.GetGenericTypeDefinition();
-
-        if (genericTypeDefinition == typeof(Task<>) || genericTypeDefinition == typeof(ValueTask<>))
-            return returnType.GetGenericArguments()[0].IsErrorOrReturnType();
-
-        return genericTypeDefinition == typeof(ErrorOr<>);
-    }
-
-    private static bool HasValidatorIn(this Type messageType, Assembly assembly) =>
-        assembly
-            .GetTypes()
-            .Where(type => !type.IsAbstract && !type.IsGenericTypeDefinition)
-            .SelectMany(type =>
-                type.GetInterfaces()
-                    .Where(i =>
-                        i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IValidator<>)
-                    )
-                    .Select(i => i.GetGenericArguments()[0])
-            )
-            .Contains(messageType);
 }
